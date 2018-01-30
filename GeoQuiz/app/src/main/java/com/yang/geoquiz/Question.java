7686c418ee2d2fd961b3c27dfacfaa70070ee6ba
byfyang <darkspherico@gmail.com>
2018-01-29 21:52:41 -0800

    first push

---
+++ GeoQuiz/app/src/main/java/com/yang/geoquiz/Question.java
@@ -0,0 +1,34 @@
+package com.yang.geoquiz;
+
+/**
+ * Created by byfyang on 1/28/18.
+ */
+
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question (int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
